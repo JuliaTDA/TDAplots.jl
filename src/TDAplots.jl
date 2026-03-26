e065533d3389d3c29d2d70f465160bb6b2316ddb
Guilherme Vituri <viturivituri@gmail.com>
2026-03-26 03:32:06 -0300

    Refactor exports in TDAplots.jl to include additional layout functions and ensure proper module usage

--- src/TDAplots.jl
+++ src/TDAplots.jl
@@ -5,6 +5,7 @@ using GLMakie
 import NetworkLayout
 using Reexport
 @reexport using TDAmapper
+using Graphs: edges
 using Chain
 using StatsBase: mean
 
@@ -16,10 +17,13 @@ export NaiveLayouts
 include("plots.jl")
 export rescale,
     colorscale,
+    metricspace_plot,
     mapper_plot,
     node_colors
 
 using MultivariateStats, ManifoldLearning
+import UMAP
+import NetworkLayout
 include("layouts.jl")
 export layout_generic,
     centroid,
@@ -30,6 +34,20 @@ export layout_generic,
     layout_ltsa,
     layout_diffmap,
     layout_tsne,
-    layout_isomap
+    layout_isomap,
+    # MultivariateStats layouts
+    layout_pca,
+    layout_kpca,
+    layout_ppca,
+    layout_fa,
+    layout_ica,
+    # UMAP layout
+    layout_umap,
+    # Graph-topology layouts
+    layout_spring,
+    layout_stress,
+    layout_spectral_graph,
+    layout_sfdp,
+    layout_shell
 
 end
